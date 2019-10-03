b88597209b31d9b58abb5cf78de3797a1e347c57
Layla Al-Hammoury <49364252+laylahammoury@users.noreply.github.com>
2019-10-03 16:06:20 +0300

    changes on js file

--- js/index.js
+++ js/index.js
@@ -1,19 +1,43 @@
-
+let users;
+let careers;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'json/Users.json', false);
     xhr.onload = function(){
-       let users =  JSON.parse(xhr.responseText)
+        users =  JSON.parse(xhr.responseText)
         // console.log($users);
-        users.forEach(function (user) {
-            document.write("    <div>\n" +
-                "        <div>\n" +
-                "            <p> " + user.name + "</p>\n" +
-                "            <p>" + user.id + "</p>\n" +
-                "            <p>" + user.careerId + "</p>\n" +
-                "            <hr>\n" +
-                "        </div>\n" +
-                "    </div>")
-        })
+
     }
     xhr.send();
+
+    let xhr2 = new XMLHttpRequest();
+    xhr.open('GET', 'json/Careers.json', false);
+    xhr2.onload = function () {
+         careers = JSON.parse(xhr2.responseText)
+        careers.forEach(function (career) {
+
+        })
+
+
+    }
+
+    users.forEach(function (user) {
+        document.write("<div>\n" +
+            "        <div>\n" +
+            "            <p>" + user.name + "</p>\n" +
+            "            <p>" + user.id + "</p>\n" +
+            "            <p>" + user.careerId + "</p>\n" +
+            "            <hr>\n" +
+            "        </div>\n" +
+            "          </div>")
+    })
+
+    careers.forEach(function (career) {
+        document.write("<div>\n" +
+            "        <div>\n" +
+            "            <p>" + career.title + "</p>\n" +
+            "            <p>" + career.id + "</p>\n" +
+            "            <hr>\n" +
+            "        </div>\n" +
+            "          </div>")
+    })
 
